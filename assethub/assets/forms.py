1ff80b857d070516565ef605aaf9c8ca61104f93
Ilya Portnov <portnov@iportnov.ru>
2017-01-04 19:46:09 +0500

    Markdown for asset descriptions.

--- assethub/assets/forms.py
+++ assethub/assets/forms.py
@@ -1,11 +1,17 @@
 
 from django import forms
 from django.forms import ModelForm
+from django.db import models
+
+from pagedown.widgets import PagedownWidget
 
 from models import Asset
 
 class AssetForm(ModelForm):
+
     class Meta:
         model = Asset
         fields = ['application', 'component', 'title', 'notes', 'image', 'data', 'url', 'version', 'tags']
+        widgets = {'notes': PagedownWidget()}
+
 
