ea1934cf02d6b7dcad00b54fd53d9ba16f2f4e61
Ilya Portnov <portnov@iportnov.ru>
2017-01-04 20:34:10 +0500

    Reject too large images as asset thumbnails.

--- assethub/assets/forms.py
+++ assethub/assets/forms.py
@@ -2,10 +2,13 @@
 from django import forms
 from django.forms import ModelForm
 from django.db import models
+from django.core.files.images import get_image_dimensions
 
 from pagedown.widgets import PagedownWidget
 
 from models import Asset
+
+MAX_IMAGE_SIZE=300
 
 class AssetForm(ModelForm):
 
@@ -13,5 +16,15 @@ class AssetForm(ModelForm):
         model = Asset
         fields = ['application', 'component', 'title', 'notes', 'image', 'data', 'url', 'version', 'tags']
         widgets = {'notes': PagedownWidget()}
+
+    def clean_image(self):
+        image = self.cleaned_data.get("image")
+        if not image:
+            raise forms.ValidationError("No thumbnail provided")
+        else:
+           w, h = get_image_dimensions(image)
+           if w > MAX_IMAGE_SIZE or h > MAX_IMAGE_SIZE:
+               raise forms.ValidationError("The image is too large: {0}x{1}; maximum size allowed is {2}px".format(w,h,MAX_IMAGE_SIZE))
+        return image
 
 
