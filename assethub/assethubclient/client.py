5806c57d2ab07036bed6d099c8ab77d785bb0ca4
Ilya Portnov <portnov@iportnov.ru>
2017-01-16 22:25:12 +0500

    Add options to client.

--- assethub/assethubclient/client.py
+++ assethub/assethubclient/client.py
@@ -1,8 +1,37 @@
 #!/usr/bin/env python
 
-from os.path import join
+from os.path import join, basename
 import requests
 import argparse
+
+class Asset(object):
+    def __init__(self, json):
+        self.dict = json
+
+    def __getattr__(self, attr):
+        return self.dict[attr]
+
+    def json(self):
+        return self.dict
+    
+    def __unicode__(self):
+        return unicode(self.dict)
+
+    def __str__(self):
+        return str(self.dict)
+
+    def __repr__(self):
+        return repr(self.dict)
+
+    def download_data(self, outpath):
+        r = requests.get(self.data)
+        r.raise_for_status()
+        with open(outpath, 'wb') as f:
+            for chunk in r.iter_content(chunk_size=1024):
+                f.write(chunk)
+
+    def get_filename(self):
+        return basename(self.data)
 
 class AssetHubClient(object):
     def __init__(self, url, application=None, component=None):
@@ -31,7 +60,7 @@ class AssetHubClient(object):
     def list(self):
         r = requests.get(self._get_url(), params=self._get_params())
         r.raise_for_status()
-        return r.json()
+        return [Asset(a) for a in r.json()]
 
 if __name__ == "__main__":
 
@@ -41,6 +70,8 @@ if __name__ == "__main__":
     parser.add_argument('-c', '--component', metavar='COMPONENT', help='Component')
     parser.add_argument('--author', metavar='USER', help='Asset author')
     parser.add_argument('-t', '--tag', metavar='TAG', help='Asset tag')
+    parser.add_argument('-J', '--json', action='store_true', help='Output data as JSON')
+    parser.add_argument('-D', '--download', action='store_true', help='Download asset data')
     args = parser.parse_args()
 
     if args.url:
@@ -52,4 +83,11 @@ if __name__ == "__main__":
     client.tag = args.tag
     client.author = args.author
     for asset in client.list():
-        print(asset['title'])
+        if args.download:
+            asset.download_data(asset.get_filename())
+            print("Downloaded " + asset.get_filename())
+        elif args.json:
+            print(asset)
+        else:
+            print(asset.title)
+
